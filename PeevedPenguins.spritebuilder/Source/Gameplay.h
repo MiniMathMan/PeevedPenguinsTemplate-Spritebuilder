4710e2b2bddca1441afcbf43f2d3f4c4cb270a59
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-04 06:17:56 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Lisa Gathard on 7/4/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
