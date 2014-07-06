1b42da51987f01e396d35f358aba630e3a5d5f30
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-06 03:12:26 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -31,7 +31,7 @@
 }
 
 // called on every touch in this scene
--(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+/*-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     CGPoint touchLocation = [touch locationInNode:_contentNode];
     
@@ -44,7 +44,7 @@
         // setup a spring joint between the mouseJointNode and the catapultArm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
     }
-}
+}*/
 
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
 {
