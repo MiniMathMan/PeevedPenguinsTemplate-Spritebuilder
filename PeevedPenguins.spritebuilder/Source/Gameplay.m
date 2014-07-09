dea7a98b11c288ea4000285b4088567764b8eadd
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-09 09:03:12 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -119,7 +119,7 @@
 
 - (void)sealRemoved:(CCNode *)seal {
     // load particle effect
-    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    CCParticleSystem *explosion = [CCBReader load:@"SealExplosion"];
     // make the particle effect clean itself up, once it is completed
     explosion.autoRemoveOnFinish = TRUE;
     // place the particle effect on the seals position
