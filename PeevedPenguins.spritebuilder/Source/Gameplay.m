a99f188dc349cf4539ebb535280a9a026eac1c5c
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-10 04:25:53 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -27,7 +27,7 @@
     _currentPenguin = nil;
     [_contentNode stopAction:_followPenguin];
     
-    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:5.f position:ccp(0, 0)];
     [_contentNode runAction:actionMoveTo];
 }
 
