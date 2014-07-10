f47e428cf77a6323466e81c7c2fcc1f6a5abdbd9
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-10 04:18:45 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -37,7 +37,7 @@
 {
     static const float MIN_SPEED = 1.f;
     // if speed is below minimum speed, assume this attempt is over
-    CCLOG("%f",ccpLength(_currentPenguin.physicsBody.velocity));
+    CCLOG(@"%f",ccpLength(_currentPenguin.physicsBody.velocity));
     if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
         [self nextAttempt];
         return;
