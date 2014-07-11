0f1234dc1a6d2118f61c61e0c4d2408c5cced5a6
MiniMathMan <Home@Lisas-MacBook-Pro-2.local>
2014-07-11 03:12:25 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -8,6 +8,7 @@
 
 #import "Gameplay.h"
 #import "CCPhysics+ObjectiveChipmunk.h"
+#import "Penguin.h"
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
@@ -17,7 +18,7 @@
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
-    CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
     CCAction *_followPenguin;
 
@@ -34,7 +35,7 @@
 // is called when CCB file has completed loading
 // is called when CCB file has completed loading
 - (void)update:(CCTime)delta
-{
+{   if (_currentPenguin.launched) {
     static const float MIN_SPEED = 5.f;
     // if speed is below minimum speed, assume this attempt is over
     if (_currentPenguin == nil) {
@@ -58,6 +59,7 @@
         [self nextAttempt];
         return;
     }
+}
 }
 
 
@@ -87,7 +89,7 @@
         // setup a spring joint between the mouseJointNode and the catapultArm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
         // create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
+        _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
         // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
         CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
         // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -126,6 +128,7 @@
         // follow the flying penguin
         _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
         [_contentNode runAction:_followPenguin];
+        _currentPenguin.launched = TRUE;
     }
     
     
--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -10,4 +10,6 @@
 
 @implementation Penguin
 
+
+
 @end
